982a502560738cceaa47eb03a2d6b3419f781921
Igor Sorokin <sorokin092@gmail.com>
2023-04-03 23:34:25 +0600

    first commit

---
+++ indexHW.js
@@ -0,0 +1,45 @@
+// let user = {};
+// user.name = prompt("What is your name?", "");
+// user.age = prompt("How old are you?", "");
+// user.sex = prompt("Are you male or female?", "");
+// console.log(user);
+
+// let num = prompt("Введите число!", "1");
+// let toNum = Number(num);
+// if (toNum === Number(num)) {
+//     alert(toNum);
+// } else {
+//     alert("Вы ввели не число! Попробуйте еще!");
+// };
+
+
+// let a = +prompt('a?', '');
+// console.log(a) // string
+// a = +a
+// console.log(a) // number
+// switch (a) {
+//     case 0:
+//         alert( 0 );
+//         break;
+//     case 1:
+//         alert( 1 );
+//         break;
+//     case 2:
+//     case 3:
+//         alert( '2,3' );
+// };
+
+// let sum = 0;
+// for (i = 1; i <=100; i++) {
+//     if (i % 2 == 0) {
+//         sum += i;
+//     }
+// }
+// console.log(sum);
+
+
+let i = 0;
+while (i < 3) {
+    alert( `number ${i}!` );
+    i++
+};
